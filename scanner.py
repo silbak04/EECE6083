59096b4ea21e6064b5bdc32e9ecb63e9484bd07f
Samir Silbak <silbak04@gmail.com>
2014-01-23 00:14:14 -0500

    [scanner] updated scanner to support most of the tokens

--- scanner.py
+++ scanner.py
@@ -1,3 +1,23 @@
+#
+# Copyright (C) 2014 by Samir Silbak
+#
+# Compiler Thoery - Scanner
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+
 import os
 import sys
 
@@ -82,15 +102,13 @@ def get_tokens():
                 get_next_char()
                 colm_pos += 1
 
-            #if (curr_token in token_table):
-                #token.append(token_table[curr_token] + ', ' + curr_token)
             if (curr_token in reserved_ids):
-                tokens.append(curr_token)
+                tokens.append(("res_id", curr_token))
             else:
-                tokens.append(("id", curr_token))
+                tokens.append(("id"    , curr_token))
 
         # check if char is a digit or a decmial point
-        if (curr_char.isdigit() or curr_char == "."):
+        elif (curr_char.isdigit() or curr_char == "."):
             curr_token = curr_char
             error = 0
 
@@ -114,25 +132,29 @@ def get_tokens():
                     get_next_char()
                     colm_pos += 1
 
-                tokens.append(("unknown_token", curr_token))
-                print "syntax error: '%s': line:%i:%i" % (curr_token, line_cnt, colm_pos)
+                tokens.append(("unknown_token_digit", curr_token))
+                print "[error]: '%s': line:%i:%i" % (curr_token, line_cnt, colm_pos)
 
             else:
+
+                # check for leading zero
                 if (curr_token[0] == "."):
-                    print "[warning]: leading zero is missing, it has been added"
+                    print "[warning]: leading zero is missing on line:%i, it has been added" % (line_cnt)
                     curr_token = '0' + curr_token
 
+                # check for trailing zero
                 if (curr_token[-1] == "."):
-                    print "[warning]: trailing zero is missing, it has been added"
+                    print "[warning]: trailing zero is missing on line:%i, it has been added" % (line_cnt)
                     curr_token += '0'
 
+                # check for type int or float
                 if ("." in curr_token):
                     tokens.append(("float", curr_token))
                 else:
                     tokens.append(("int"  , curr_token))
 
         # check for strings
-        if (curr_char == "\""):
+        elif (curr_char == "\""):
             curr_token = curr_char
 
             # keep fetching the next char as long as the next char
@@ -149,69 +171,75 @@ def get_tokens():
                 tokens.append(("string", curr_token))
 
             else:
-                print "missing end quote: line:%i:%i" % (line_cnt, colm_pos)
-                tokens.append(("uknown_token", curr_token))
+                print "[error]: missing quote: line:%i:%i" % (line_cnt, colm_pos)
+                tokens.append(("unknown_token_str", curr_token))
 
-        #else:
+        else:
 
-        # ignore white space
-        if (curr_char == " "): continue
+            # ignore white space
+            # i don't think i need this
+            if (curr_char.isspace()): continue
 
-        # check for operators
-        if (curr_char in operators):
-            curr_token = curr_char
+            # check for operators - <, >, !, :
+            elif (curr_char in operators):
+                curr_token = curr_char
 
-            if (next_char == "="):
-                curr_token += next_char
-                get_next_char()
-                colm_pos += 1
-                tokens.append((token_table[curr_token], curr_token))
+                # check for <=, >=, !=, :=
+                if (next_char == "="):
+                    curr_token += next_char
+                    get_next_char()
+                    colm_pos += 1
+                    tokens.append((token_table[curr_token], curr_token))
 
-            if (next_char in operators):
-                curr_token += next_char
-                get_next_char()
-                colm_pos += 1
-                tokens.append(("unknown_token", curr_token))
+                # check for unsupported operators: <<, >>, !!, ::
+                if (next_char in operators):
+                    curr_token += next_char
+                    get_next_char()
+                    colm_pos += 1
+                    tokens.append(("unknown_token_op", curr_token))
 
-        # check for division or comment
-        if (curr_char == "/"):
-            curr_token = curr_char
+            # check for division or comment
+            elif (curr_char == "/"):
+                curr_token = curr_char
 
-            #if ((next_char.isdigit() or next_char.isalpha()) and (next_char != "\n")):
-            if (next_char != "/" and next_char != "\n"):
-                colm_pos += 1
-                tokens.append((token_table[curr_token], curr_token))
+                #if ((next_char.isdigit() or next_char.isalpha()) and (next_char != "\n")):
+                if (next_char != "/" and next_char != "\n"):
+                    colm_pos += 1
+                    tokens.append((token_table[curr_token], curr_token))
 
-            elif (next_char == "/"):
-                while (next_char != "\n"):
+                elif (next_char == "/"):
+                    while (next_char != "\n"):
+                        curr_token += next_char
+                        get_next_char()
+                        colm_pos += 1
+
+                    # most likely token_table will be gone, much simplier to just
+                    # pass in the strings directly
+                    tokens.append((token_table[curr_token[0:2]], curr_token))
+
+            # check for escape sequences
+            elif (curr_char == "\\"):
+                curr_token = curr_char
+
+                if (next_char in ["t", "n"] and next_char != "\n"):
                     curr_token += next_char
                     get_next_char()
                     colm_pos += 1
 
-                # most likely token_table will be gone, much simplier to just
-                # pass in the strings directly
-                tokens.append((token_table[curr_token[0:2]], curr_token))
+                else:
+                    print "[error]: escape sequence, %s is not supported: line:%i:%i" % (curr_token, line_cnt, colm_pos)
 
-        if (curr_char == "\\"):
-            curr_token = curr_char
+                tokens.append((token_table[curr_token], curr_token))
 
-            if (next_char in ["t", "n"] and next_char != "\n"):
-                curr_token += next_char
-                get_next_char()
-                colm_pos += 1
-
+            elif (curr_char in token_table):
+                tokens.append((token_table[curr_char], curr_char))
             else:
-                print "escape sequence, %s is not supported: line:%i:%i" % (curr_token, line_cnt, colm_pos)
-
-            tokens.append((token_table[curr_token], curr_token))
-
-            #if (curr_char in token_table):
-            #    print "wtf"
-            #    tokens.append(token_table[curr_char] + ", " + curr_char)
+                tokens.append(("unknown_token_tok", curr_char))
 
         update_line_cnt()
 
-    print tokens
+    for token in tokens:
+        print token
 
 def main():
 
